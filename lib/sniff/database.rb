24f2c7c157e9227da16906cd666009793847675b
Derek Kastner <dkastner@gmail.com>
2010-07-02 16:23:44 -0400

    Refactored database to allow schema/db create/drop

--- lib/sniff/database.rb
+++ lib/sniff/database.rb
@@ -4,18 +4,52 @@ module Sniff
   module Database
     extend self
 
-    def connect(base_dir)
+    def connect(base_dir, options = {})
+      options[:load_data] = true if options[:load_data].nil?
+
       require 'active_record'
       require 'sqlite3'
-      db_dir = File.join(base_dir, 'db')
-      FileUtils.mkdir_p db_dir
-      ActiveRecord::Base.establish_connection(
-        :adapter => 'sqlite3',
-        :database => File.join(db_dir, 'emitter_data.sqlite3'),
-        :pool => 5,
-        :timeout => 5000
-      )
+      FileUtils.mkdir_p db_path
+      db_drop
+      db_create
+      load_data if options[:load_data]
       load_models
+    end
+
+    def ar_connect
+      ActiveRecord::Base.establish_connection config
+    end
+
+    def config
+      { :adapter => 'sqlite3',
+        :database => db_file_path,
+        :pool => 5,
+        :timeout => 5000 }
+    end
+
+    def db_path
+      File.join(Sniff.root, 'db')
+    end
+    def db_file_path
+      File.join(db_path, 'emitter_data.sqlite3')
+    end
+
+    def db_drop
+      FileUtils.rm db_file_path if File.exists?(db_file_path)
+    end
+
+    def db_create
+      ar_connect
+      ActiveRecord::Base.connection
+    end
+
+    def load_schema
+      file = "#{Sniff.root}/db/schema.rb"
+      load(file)
+    end
+
+    def load_data
+      # TODO
     end
 
   private
