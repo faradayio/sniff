28b4298bec1bf97711c06b64b3a40455fca50e40
Derek Kastner <dkastner@gmail.com>
2010-07-07 17:18:53 -0400

    Added interactive console that loads test data

--- lib/sniff/tasks.rb
+++ lib/sniff/tasks.rb
@@ -1,8 +1,22 @@
-require 'active_record'
-require 'active_record/railties/databases.rake'
-
 task :environment do
   ENV['SCHEMA'] = File.join Sniff.root, 'db', 'schema.rb'
   ENV['FIXTURES_PATH'] = File.join Sniff.root, 'test', 'fixtures'
 end
 
+task :console do
+  require 'sniff'
+  cwd = Dir.pwd
+  DB_DIR = File.join cwd, 'features'
+  FileUtils.mkdir_p(DB_DIR)
+  Sniff::Database.init DB_DIR
+
+  $:.unshift File.join(cwd, 'lib')
+  Dir[File.join(DB_DIR, 'lib', '*.rb')].each do |lib|
+    puts "Loading #{lib}"
+    require lib
+  end
+
+  require 'irb'
+  ARGV.clear
+  IRB.start
+end
