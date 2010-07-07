1e8fc829b17e9fb43094217b82305d0fd4fb11b6
Derek Kastner <dkastner@gmail.com>
2010-07-07 18:12:01 -0400

    Simplified console task

--- lib/sniff/tasks.rb
+++ lib/sniff/tasks.rb
@@ -6,15 +6,7 @@ end
 task :console do
   require 'sniff'
   cwd = Dir.pwd
-  DB_DIR = File.join cwd, 'features'
-  FileUtils.mkdir_p(DB_DIR)
-  Sniff::Database.init DB_DIR
-
-  $:.unshift File.join(cwd, 'lib')
-  Dir[File.join(DB_DIR, 'lib', '*.rb')].each do |lib|
-    puts "Loading #{lib}"
-    require lib
-  end
+  Sniff::Database.init cwd
 
   require 'irb'
   ARGV.clear
