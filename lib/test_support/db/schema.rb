589eb711e1bfad7e6f82bcb6e30a2a756f3145e2
Derek Kastner <dkastner@gmail.com>
2010-07-09 12:25:23 -0400

    Added back fallbacks

--- lib/test_support/db/schema.rb
+++ lib/test_support/db/schema.rb
@@ -1,15 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
+require 'sniff/database'
 
-ActiveRecord::Schema.define(:version => 20100701223036) do
+Sniff::Database.define_schema do
   create_table "census_divisions", :primary_key => "number", :id => false, :force => true do |t|
     t.string   "number"
     t.string   "name"
@@ -59,6 +50,13 @@ ActiveRecord::Schema.define(:version => 20100701223036) do
     t.string   "egrid_region_name"
     t.datetime "updated_at"
     t.datetime "created_at"
+  end
+
+  create_table "fallbacks", :force => true do |t|
+    t.string   "name"
+    t.text     "values"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "genders", :primary_key => "name", :id => false, :force => true do |t|
