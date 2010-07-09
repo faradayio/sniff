19ba559d3cd2c2f1fa7c4636828a66ee05c9a7fa
Derek Kastner <dkastner@gmail.com>
2010-07-09 12:20:58 -0400

    Added ability to load rails plugins - change interface from calling Sniff::Database.init to Sniff.init

--- lib/sniff.rb
+++ lib/sniff.rb
@@ -4,6 +4,22 @@ module Sniff
   def root 
     File.join(File.dirname(__FILE__), '..')
   end
+
+  def init(local_root, options = {})
+    require 'active_record'
+    require 'sqlite3'
+
+    load_plugins
+
+    Sniff::Database.init local_root, options
+  end
+
+  def load_plugins
+    Dir[File.join(Sniff.root, 'vendor', '**', 'init.rb')].each do |pluginit|
+      $:.unshift File.join(File.dirname(pluginit), 'lib')
+      load pluginit
+    end
+  end
 end
 
 require 'characterizable'
--- spec/lib/sniff/database_spec.rb
+++ spec/lib/sniff/database_spec.rb
@@ -3,16 +3,19 @@ require 'fileutils'
 
 describe Sniff::Database do
   describe '#connect' do
-    before :each do
+    before :all do
       @db_path = File.join(File.dirname(__FILE__), '..', '..', '..')
-      Sniff::Database.init(@db_path)
+      Sniff.init(@db_path)
       @sqlite_path = File.join(@db_path, 'db', 'emitter_data.sqlite3') 
     end
-    after :each do
+    after :all do
       FileUtils.rm_f(@sqlite_path)
     end
     it 'should create an sqlite database in the given directory' do
       File.exists?(@sqlite_path).should be_true
+    end
+    it 'should load data' do
+      ZipCode.count.should > 0
     end
   end
 end
