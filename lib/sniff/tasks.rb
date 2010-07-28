289012657860835a79e138116e8fcb3b919c8b42
Derek Kastner <dkastner@gmail.com>
2010-07-28 09:22:46 -0400

    Don't need environment task

--- lib/sniff/tasks.rb
+++ lib/sniff/tasks.rb
@@ -1,8 +1,3 @@
-task :environment do
-  ENV['SCHEMA'] = File.join Sniff.root, 'db', 'schema.rb'
-  ENV['FIXTURES_PATH'] = File.join Sniff.root, 'test', 'fixtures'
-end
-
 task :console do
   require 'sniff'
   cwd = Dir.pwd
