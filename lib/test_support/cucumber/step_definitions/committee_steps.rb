d5aae75e2c2d5c605693352ce5d4e2ed53cd7ab7
Derek Kastner <dkastner@gmail.com>
2010-08-03 17:16:43 -0400

    Added a raft of new functionality for committee cukes

--- lib/test_support/cucumber/step_definitions/committee_steps.rb
+++ lib/test_support/cucumber/step_definitions/committee_steps.rb
@@ -1,15 +1,22 @@
-Given /^a purchase emitter$/ do
-  @activity = PurchaseRecord
+Given /^a (.+) emitter$/ do |name|
+  name = name.gsub(/\s+/,'_').camelize + 'Record'
+  @activity = name.constantize
+  @characteristics ||= {}
 end
 
 Given /^(a )?characteristic "(.*)" of "(.*)"$/ do |_, name, value|
-  @characteristics ||= {}
-
   if name =~ /\./
     model_name, attribute = name.split /\./
-    model = model_name.singularize.camelize.constantize
+    model = begin
+      model_name.singularize.camelize.constantize
+    rescue NameError
+      association = @activity.reflect_on_association model_name.to_sym
+      association.klass
+    end
     value = model.send "find_by_#{attribute}", value
     @characteristics[model_name.to_sym] = value
+  elsif name == 'timeframe' and value.present?
+    @characteristics[name.to_sym] = Timeframe.interval value
   else
     value = coerce_value(value)
     @characteristics[name.to_sym] = value
@@ -19,16 +26,27 @@ end
 When /^the "(.*)" committee is calculated$/ do |committee_name|
   @decision ||= @activity.decisions[:emission]
   @committee = @decision.committees.find { |c| c.name.to_s == committee_name }
-  @report = @committee.report(@characteristics, [])
-  @characteristics[committee_name.to_sym] = @report.conclusion
+  args = [@characteristics]
+  if @characteristics[:timeframe]
+    args << [@characteristics[:timeframe]]
+  else
+    args << []
+  end
+  @report = @committee.report *args
+  @characteristics[committee_name.to_sym] = @report.andand.conclusion
 end
 
 Then /^the committee should have used quorum "(.*)"$/ do |quorum|
+  raise "Missing report for committee #{@committee.name}" if @report.nil?
   @report.quorum.name.should == quorum
 end
 
-Then /^the conclusion of the committee should be "(.+)"$/ do |conclusion|
-  compare_values(@report.conclusion, conclusion)
+Then /^the conclusion of the committee should be "(.*)"$/ do |conclusion|
+  compare_values(@report.andand.conclusion, conclusion)
+end
+
+Then /^the conclusion of the committee should be nil$/ do
+  compare_values(@report.andand.conclusion, nil)
 end
 
 Then /^the conclusion of the committee should include a key of (.*) and value (.*)$/ do |key, value|
@@ -56,6 +74,11 @@ Then /^the conclusion of the committee should have a record identified with "(.*
   records = @report.conclusion
   record = records.send("find_by_#{id_field}", id)
   coerce_value(record.send(field)).should == coerce_value(value)
+end
+
+Then /^the conclusion of the committee should have a record with "([^"]*)" equal to "([^"]*)"$/ do |field, value|
+  record = @report.conclusion
+  compare_values(coerce_value(record.send(field)),value)
 end
 
 Then /^the conclusion of the committee should include a key of "(.*)" and subvalue "(.*)" of "(.*)" and subvalue "(.*)" of "(.*)"$/ do |key, subkey1, subvalue1, subkey2, subvalue2|
--- lib/test_support/cucumber/support/values.rb
+++ lib/test_support/cucumber/support/values.rb
@@ -1,5 +1,14 @@
+require 'date'
+
 def coerce_value(value)
-  if value =~ /\d+\.\d+/
+  # what is this, PHP?
+  if value.nil?
+    nil
+  elsif value == 'true'
+    true
+  elsif value == 'false'
+    false
+  elsif value =~ /\d+\.\d+/
     value.to_f
   elsif value =~ /^\d+$/
     value.to_i
@@ -9,7 +18,12 @@ def coerce_value(value)
 end
 
 def compare_values(a, b)
-  if b =~ /\d+\.\d+/
+  if b.nil?
+    a.should be_nil
+  elsif a.is_a? Date or a.is_a? Time
+    b = Date.parse b
+    a.should == b
+  elsif b =~ /\d+\.\d+/
     b = b.to_f
     a.should be_close(b, 0.00001)
   elsif b =~ /^\d+$/
