09cdb110c3a730dc2ce94ef5766fddabf3c16ee9
Ian Hough <ian@brighterplanet.com>
2011-01-11 12:27:23 -0500

    Improve data import test syntax for models that begin with a vowel

--- lib/test_support/cucumber/step_definitions/data_steps.rb
+++ lib/test_support/cucumber/step_definitions/data_steps.rb
@@ -1,4 +1,4 @@
-Given /^a "([^\"]*)" data import fetches results listed in "(.*)"$/ do |model, file_name|
+Given /^an? "([^\"]*)" data import fetches results listed in "(.*)"$/ do |model, file_name|
   @data_import_model = model.constantize
   table_name = @data_import_model.table_name
 
