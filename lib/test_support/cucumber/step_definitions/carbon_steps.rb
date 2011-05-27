0e3ea9d842f72ac4faccb21ace681e3f7ca71270
Ian Hough <ian@brighterplanet.com>
2011-05-27 14:12:57 -0400

    Derek's fixes to emission compliance tests

--- lib/test_support/cucumber/step_definitions/carbon_steps.rb
+++ lib/test_support/cucumber/step_definitions/carbon_steps.rb
@@ -53,7 +53,7 @@ end
 
 Then /^the emission value should be within "([\d\.]+)" kgs of "([\d\.]+)"$/ do |cusion, emissions|
   @emission.should_not be_nil
-  @emission.should be_close(emissions.to_f, cusion.to_f)
+  @emission.should be_within(cusion.to_f).of(emissions.to_f)
 end
 
 Then /^the calculation should have used committees "(.*)"$/ do |committee_list|
@@ -64,17 +64,26 @@ Then /^the calculation should have used committees "(.*)"$/ do |committee_list|
 end
 
 Then /^the calculation should comply with standards? "(.*)"$/ do |standard_list|
-  standards = standard_list.split(/,\s*/).map(&:to_sym)
-  (@emission.compliance - standards).should be_empty
+  standards = Set.new standard_list.split(/,\s*/).map(&:to_sym)
+  compliance = Set.new @activity.deliberations[:emission].compliance
+  unless standards.empty?
+    compliance.should_not be_empty, 'Expected calculation to comply with some standards, but it complied with none'
+  end
+  exclusive_list = (compliance ^ standards)
+  exclusive_list.should be_empty, "Calculation did not comply with #{(standards - compliance).to_a.inspect})"
 end
 
 Then /^the calculation should not comply with standards? "(.*)"$/ do |standard_list|
-  standards = standard_list.split(/,\s*/).map(&:to_sym)
-  (@emission.compliance - standards).should == @emission.compliance
+  standards = Set.new standard_list.split(/,\s*/).map(&:to_sym)
+  compliance = Set.new @activity.deliberations[:emission].compliance
+  unless compliance.empty?
+    diff_list = (standards - compliance)  # s - c = set of anything in s that is not in c
+    diff_list.should be(standards), "Calculation should not have complied with #{(standards - diff_list).to_a.inspect}"
+  end
 end
 
 Then /^the (.+) committee should be close to "([^,]+)", \+\/-"(.+)"$/ do |committee, value, cusion|
-  @characteristics[committee.to_sym].to_f.should be_close(value.to_f, cusion.to_f)
+  @characteristics[committee.to_sym].to_f.should be_within(cusion.to_f).of(value.to_f)
 end
 
 Then /^the (.+) committee should be exactly "(.*)"$/ do |committee, value|
