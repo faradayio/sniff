7e1ddeff1070ebef13e661d847a120e826584231
Derek Kastner <dkastner@gmail.com>
2012-02-01 21:39:37 -0500

    Allow tests on a specified decision

--- lib/test_support/cucumber/step_definitions/leap_steps.rb
+++ lib/test_support/cucumber/step_definitions/leap_steps.rb
@@ -1,3 +1,7 @@
+Given %r{a decision is to be made on "(.*)"} do |decision|
+  @decision = @activity.decisions[decision.to_sym]
+end
+
 Then /trace the calculation/ do
   @activity_instance.class.send :include, Leap::Trace
   @activity_instance.trace_report
